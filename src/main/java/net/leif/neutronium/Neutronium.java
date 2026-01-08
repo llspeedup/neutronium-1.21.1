80993adaa3e475187df562f333a1deab824a39fe
llspeedup <leifbschroder1@gmail.com>
2026-01-08 17:19:43 +1000

    Added uranium ore, working, along with an unenriched uranium smelting, also working. TO DO: uranium enricher block / mechanic deepslate uranium ore

--- src/main/java/net/leif/neutronium/Neutronium.java
+++ src/main/java/net/leif/neutronium/Neutronium.java
@@ -2,6 +2,7 @@ package net.leif.neutronium;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.leif.neutronium.block.ModBlocks;
 import net.leif.neutronium.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class Neutronium implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/leif/neutronium/block/ModBlocks.java
@@ -0,0 +1,35 @@
+package net.leif.neutronium.block;
+
+import net.leif.neutronium.Neutronium;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    private static final Block URANIUM_ORE = registerBlock("uranium_ore", new Block(AbstractBlock.Settings.create()
+            .strength(3f)
+            .requiresTool()
+            .sounds(BlockSoundGroup.STONE)
+    ));
+
+
+
+    private static void registerBlockItem(String name, Block block){
+        Registry.register(Registries.ITEM, Identifier.of(Neutronium.MOD_ID, name), new BlockItem(block, new Item.Settings()));
+    }
+
+    private static Block registerBlock(String name, Block block){
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(Neutronium.MOD_ID, name), block);
+    }
+
+    public static void registerModBlocks(){
+        Neutronium.LOGGER.info("Registering items for " + Neutronium.MOD_ID);
+    }
+}
